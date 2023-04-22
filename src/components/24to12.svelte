d79c6830cd067a4b23de5ce7b7834e5e1cf0856e
Jessy BREYNE <jessybreyne@protonmail.com>
2023-04-22 10:28:04 +0200

    Exercice convert 24 vers 12

--- src/components/24to12.svelte
+++ src/components/24to12.svelte
@@ -1,57 +1,66 @@
 <!-- TimeConversion.svelte -->
 <script lang="ts">
+  export let url;
+
+  import { getLangFromUrl, useTranslations } from '../i18n/utils';
+
+  const lang = getLangFromUrl(url);
+  const t = useTranslations(lang);
+
   let hour24 = "";
   let userInput = "";
   let hour12 = "";
-  let ampm = "";
+  let ampm = "PM";
   let success = false;
   let error = false;
 
   function generateRandomHour24() {
-    const randomHour = Math.floor(Math.random() * 24).toString().padStart(2, "0");
-    hour24 = randomHour;
-  }
+  const randomHour = Math.floor(Math.random() * 24).toString().padStart(2, "0");
+  hour24 = randomHour;
+}
+
 
   function convertTo12HourFormat() {
-    const hour = parseInt(hour24, 10);
-    ampm = hour >= 12 ? "PM" : "AM";
+  const hour = parseInt(hour24, 10);
 
-    if (hour === 0) {
-      hour12 = `12`;
-    } else if (hour > 12) {
-      hour12 = `${(hour - 12).toString().padStart(2, "0")}`;
-    } else {
-      hour12 = `${hour.toString().padStart(2, "0")}`;
-    }
+  if (hour === 0) {
+    hour12 = `12`;
+    ampm = "AM";
+  } else if (hour > 12) {
+    hour12 = `${(hour - 12).toString()}`;
+    ampm = "PM";
+  } else {
+    hour12 = `${hour.toString()}`;
+    ampm = hour === 12 ? "PM" : "AM";
   }
+}
 
-  function checkAnswer() {
-    const userInputHour = parseInt(userInput, 10);
 
-    if (userInputHour <= 12) {
-      const userInput24 = (userInputHour % 12).toString().padStart(2, "0");
-      const userInput24pm = ((userInputHour % 12) + 12).toString().padStart(2, "0");
-      success = userInput24 === hour24 || userInput24pm === hour24;
-    } else {
-      success = false;
-    }
-    error = !success;
-    if (success) {
-      userInput = "";
-      generateRandomHour24();
-      convertTo12HourFormat();
-    }
+function checkAnswer() {
+  const inputHour = parseInt(userInput, 10);
+
+  success = parseInt(hour12, 10) === inputHour && (inputHour >= 1 && inputHour <= 12);
+  error = !success;
+
+  if (success) {
+    userInput = "";
+    generateRandomHour24();
+    convertTo12HourFormat();
   }
+}
 
-  function onInputChange(event: Event) {
-    success = false;
-    error = false;
-    userInput = (event.target as HTMLInputElement).value;
 
-    if (userInput.length === 2) {
-      checkAnswer();
-    }
+
+function onInputChange(event: Event) {
+  success = false;
+  error = false;
+  userInput = (event.target as HTMLInputElement).value;
+
+  if (userInput.length === 2) {
+    checkAnswer();
   }
+}
+
 
   generateRandomHour24();
   convertTo12HourFormat();
@@ -66,20 +75,38 @@
     gap: 1rem;
   }
   input {
-    width: 50px;
-    outline-color: initial;
-    background-color: grey;
+    width: 70px;
+    height: 50px;
+    box-shadow: none;
+    border: 2px solid white;
+    transition: all 0.3s ease-in-out;
+    font-size: 18px;
+    padding: 5px 15px;
+    background: none;
     color: white;
-    border: 1px solid blue;
+    text-align: center;
+  }
+  input:focus {
+    outline: none;
+    border-color: var(--primary);
+  }
+  input:hover {
+    border-color: var(--primary-hover);
   }
   .error {
     color: red;
   }
+  .success {
+    color: green;
+  }
+  h3 {
+    font-size: 3rem;
+  }
 </style>
 
 <div class="container">
-  <p>24h format (HH): {hour24}</p>
-  <label for="userInput">12h format (hh):</label>
+  <h3>{hour24}h </h3>
+  <label for="userInput">12h format (HH):</label>
   <input
     type="text"
     id="userInput"
@@ -88,8 +115,8 @@
     maxlength="2"
   />
   {#if success}
-    <p>Success! Try another one.</p>
+    <p class="success">{t('success')}</p>
   {:else if error}
-    <p class="error">Incorrect! Please try again.</p>
+    <p class="error">{t('error')}</p>
   {/if}
 </div>
